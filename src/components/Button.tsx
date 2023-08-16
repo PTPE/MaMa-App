0d50eaff6cd5aaf09a7fd9eec0c1fc4ad245fce2
YuChieh <winnie524722@gmail.com>
2023-08-16 19:19:45 +0800

    feat:完成Homepage

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+type buttonProps = {
+  children: React.ReactNode;
+} & stylesProp;
+
+type stylesProp = {
+  styles?: {
+    color?: string;
+    backgroundColor?: string;
+    padding?: string;
+    fontSize?: string;
+    fontWeight?: string;
+  };
+};
+
+function Button(props: buttonProps) {
+  const mergesStyles = { ...Button.defaultProps.styles, ...props.styles };
+  return <Buttons styles={mergesStyles}>{props.children}</Buttons>;
+}
+
+Button.defaultProps = {
+  styles: {
+    color: "black",
+    backgroundColor: "#FFCC29",
+    fontSize: "24px",
+    padding: "10px 20px",
+    fontWeight: "500",
+  },
+};
+
+export default Button;
+
+const Buttons = styled.button<stylesProp>`
+  color: ${(props) => props.styles?.color};
+  background: ${(props) => props.styles?.backgroundColor};
+  font-size: ${(props) => props.styles?.fontSize};
+  padding: ${(props) => props.styles?.padding};
+  font-weight: ${(props) => props.styles?.fontWeight};
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  &:hover {
+    background: #ffc978;
+  }
+`;
--- src/pages/Hompage.tsx
+++ src/pages/Hompage.tsx
@@ -1,3 +1,41 @@
+import DogIcon from "../components/DogIcon";
+import styled from "styled-components";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
+
 export default function Hompage() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Title>
+        <span>MaMa</span> APP
+      </Title>
+      <DogIcon width={250} height={250} />
+      <Link to="app">
+        <Button
+          styles={{
+            padding: "20px 60px",
+            fontWeight: "600",
+          }}
+        >
+          Start
+        </Button>
+      </Link>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 60px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  span {
+    color: #ffcc29;
+  }
+`;
