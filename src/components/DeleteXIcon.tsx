0b1657e419ad234d6ab2c762c2dfbd4ae3caac04
YuChieh <winnie524722@gmail.com>
2023-08-25 14:50:16 +0800

    feat: 建立ModalContainer

---
+++ src/components/DeleteXIcon.tsx
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export default function DeleteXIcon() {
+  return (
+    <Container>
+      <Line className="top" />
+      <Line className="bottom" />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 15px;
+  height: 15px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+const Line = styled.div`
+  position: absolute;
+  display: block;
+  width: 100%;
+  height: 2px;
+  background: #002a6a;
+
+  &.top {
+    transform: rotate(-45deg);
+  }
+  &.bottom {
+    transform: rotate(45deg);
+  }
+`;
